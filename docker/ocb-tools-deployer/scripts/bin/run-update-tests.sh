f54b51926dfdfa126103da055bd7ccb696b3bb87
stp <stp@dbc.dk>
2016-01-12 09:08:24 +0000

    Done some work on us #1762: Update: Docker-image til deploy af Update til system-tests og stresstests. Afh: #1761.
    Changed log files in volumes.
    
    
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@96002 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
+++ docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
@@ -7,9 +7,10 @@ env
 
 cd ${HOME}/opencat-business
 
-ocb-test.sh run --application Update --summary --remote --config settings
+ocb-test.sh run --application Update --summary --remote --config settings basis-delete-common-record-holdings-with-002-links
 cd -
 
 mkdir -p results
 
 cp opencat-business/*.log results/.
+cp -r opencat-business/target/* results/.
