fcea84ea09439eb6ed02b1c189dd55c13f334d94
Anders Thrane Michelsen <atm@dbc.dk>
2016-09-26 14:07:29 +0000

    Docker fikses
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@104466 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
+++ docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
@@ -7,7 +7,7 @@ ocb-test.sh run --application Update --summary --remote --config settings
 cd -
 
 cd ${OCB_USER_HOME}
-mkdir -p results
+mkdir -p /data/logs/results
 
-cp ${OCB_USER_HOME}/opencat-business/*.log results/.
-cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-*.xml results/.
+cp ${OCB_USER_HOME}/opencat-business/*.log /data/logs/results/.
+cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-*.xml /data/logs/results/.
