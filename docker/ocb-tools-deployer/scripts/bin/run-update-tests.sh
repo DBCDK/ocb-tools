4549873fc7d4404b2407208cd1e56b0abca71099
Morten von Staffeldt <mvs@dbc.dk>
2016-09-27 14:23:03 +0000

    deperation to fix the docker issues
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@104524 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
+++ docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
@@ -6,7 +6,7 @@ generate_settings.sh
 ocb-test.sh run --application Update --summary --remote --config settings
 cd -
 
-cp ${OCB_USER_HOME}/opencat-business/*.log results/.
-cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-*.xml results/.
+cp ${OCB_USER_HOME}/opencat-business/*.log /results/
+cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-ocb-tests.xml /results/
 
 cd -
