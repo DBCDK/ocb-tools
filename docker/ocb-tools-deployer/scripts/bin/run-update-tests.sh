1da8a8f7a0fa9f5547dab38b39fd73c6b38bbf6b
Anders Thrane Michelsen <atm@dbc.dk>
2016-09-27 10:35:36 +0000

    Docker stuff
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@104493 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
+++ docker/ocb-tools-deployer/scripts/bin/run-update-tests.sh
@@ -6,8 +6,10 @@ generate_settings.sh
 ocb-test.sh run --application Update --summary --remote --config settings
 cd -
 
-cd ${OCB_USER_HOME}
-mkdir -p /data/logs/results
+cd ${LOG_DIR}
+mkdir -p results
 
-cp ${OCB_USER_HOME}/opencat-business/*.log /data/logs/results/.
-cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-*.xml /data/logs/results/.
+cp ${OCB_USER_HOME}/opencat-business/*.log results/.
+cp ${OCB_USER_HOME}/opencat-business/target/surefire-reports/TEST-*.xml results/.
+
+cd -
