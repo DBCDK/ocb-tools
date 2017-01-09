692fae24d8ef29a8285d6ffd6061c0d78aac3c80
Morten von Staffeldt <mvs@dbc.dk>
2017-01-09 14:05:59 +0000

    changed dockerfile to getLastSuccesfull from the right place
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@108534 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/download-artifacts.sh
+++ docker/ocb-tools-deployer/download-artifacts.sh
@@ -2,8 +2,8 @@
 
 ARTIFACTS_DIR=artifacts
 
-OCB_TOOLS_ARTIFACT_URL=http://is.dbc.dk/job/ocb-tools-head-guesstimate/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tgz
-OPENCAT_BUSINESS_ARTIFACT_URL=http://is.dbc.dk/job/opencat-business-guesstimate-head/lastSuccessfulBuild/artifact/deploy/opencat-business.tgz
+OCB_TOOLS_ARTIFACT_URL=http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz
+OPENCAT_BUSINESS_ARTIFACT_URL=http://is.dbc.dk/job/opencat-business-head/lastSuccessfulBuild/artifact/deploy/opencat-business.tar.gz
 
 if [ -d "$ARTIFACTS_DIR" ]; then
     rm -rf $ARTIFACTS_DIR
