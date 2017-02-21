0df794fd301d94e6a12ee69929948d4d7daa76a4
Morten von Staffeldt <mvs@dbc.dk>
2017-02-21 12:30:19 +0000

    created new bash file to get rid of the shell scripting on jenkins
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@110307 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ bin/deploy-and-push-dockerimage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+function die() {
+  echo "Error:" "$@"
+  exit 1
+}
+function deployPackage () {
+  cd target/dist
+  tar -czf ocb-tools-1.0.0.tar.gz ocb-tools-1.0.0 || die "failed to deploy package"
+  cd -
+}
+
+function sanityCheckInput(){
+  if [ ${USER} != "isworker" ]; then
+    die "This program is only meant to run on an Jenkins node"
+  fi
+}
+
+function tagAndPushToArty () {
+  TAG_LATEST="docker-i.dbc.dk/ocb-tools-deployer:latest"
+  docker build --no-cache -t ${TAG_LATEST} -f docker/ocb-tools-deployer/Dockerfile docker/ocb-tools-deployer/\
+  --label=svn="${SVN_REVISION}"\
+  --label=user="${USER}"\
+  --label=jobname="${JOB_NAME}"\
+  --label=buildnumber="${BUILD_NUMBER}"\ || die "Building "${TAG_LATEST}" failed"
+  docker push "$TAG_LATEST"				|| die "failed to push $TAG_LATEST"
+  docker rmi "$TAG_LATEST"				|| die "failed to remove image $TAG_LATEST"
+}
+
+
+set -e
+#sanityCheckInput
+deployPackage
+tagAndPushToArty
