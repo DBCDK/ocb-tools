1206499d0c0d6b1166080b68e564b7924cc07e52
Anders Thrane Michelsen <atm@dbc.dk>
2017-04-28 12:27:37 +0000

    Merging build and update services
    git-svn-id: https://svn.dbc.dk/repos/ocb-tools/trunk@112749 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- docker/ocb-tools-deployer/scripts/bin/generate_settings.sh
+++ docker/ocb-tools-deployer/scripts/bin/generate_settings.sh
@@ -5,6 +5,7 @@ set -e
 mkdir -p ${OCB_USER_HOME}/.ocb-tools
 cat << EOF > ${OCB_USER_HOME}/.ocb-tools/settings.properties
 updateservice.url = http://${UPDATE_SERVICE_PORT_8080_TCP_ADDR}:${UPDATE_SERVICE_PORT_8080_TCP_PORT}
+buildservice.url = http://${UPDATE_SERVICE_PORT_8080_TCP_ADDR}:${UPDATE_SERVICE_PORT_8080_TCP_PORT}
 
 rawrepo.jdbc.driver = org.postgresql.Driver
 rawrepo.jdbc.conn.url = jdbc:postgresql://${RAWREPO_PORT_5432_TCP_ADDR}:${RAWREPO_PORT_5432_TCP_PORT}/${RAWREPO_DBNAME}
