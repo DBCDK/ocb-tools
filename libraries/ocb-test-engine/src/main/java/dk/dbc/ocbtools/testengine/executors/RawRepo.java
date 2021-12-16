5a12893198f4dd9826be57644df8858f0aeeaebe
Anders Thrane Michelsen <atm@dbc.dk>
2021-12-16 10:17:34 +0100

    Remove records_cache yet another place
    Auditors: akri, mib, jbr, hwha

--- libraries/ocb-test-engine/src/main/java/dk/dbc/ocbtools/testengine/executors/RawRepo.java
+++ libraries/ocb-test-engine/src/main/java/dk/dbc/ocbtools/testengine/executors/RawRepo.java
@@ -256,7 +256,6 @@ public class RawRepo {
                 JDBCUtil.update(conn, "DELETE FROM records");
                 JDBCUtil.update(conn, "DELETE FROM records_archive");
                 JDBCUtil.update(conn, "DELETE FROM records_summary");
-                JDBCUtil.update(conn, "DELETE FROM records_cache");
                 JDBCUtil.update(conn, "DELETE FROM queue");
                 JDBCUtil.update(conn, "DELETE FROM jobdiag");
 
